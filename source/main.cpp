32b5fe2ba6d5ad82d86ece16853a4e95c4182682
MarieRoald <roald.marie@gmail.com>
2016-04-26 13:30:32 +0200

    Created empty main

---
+++ source/main.cpp
@@ -0,0 +1,11 @@
+#include "Eigen/Eigen"
+#include "Eigen/Geometry"
+#include "Sophus/SE3.hpp"
+#include "opencv2/opencv.hpp"
+#include "opencv2/core/eigen.hpp"
+#include "opencv2/viz.hpp"
+
+int main()
+{
+
+}
