8016f8f41bf2ea7c318f3011bc26dc9fe29f83fc
Torbjørn N. Høiland <t_hoiland@hotmail.com>
2016-05-03 01:03:08 +0200

    now saving condefects, bug: largest_C_index > contours.size() at times
    Added if check - might need to fix later.

--- source/FindHull.cpp
+++ source/FindHull.cpp
@@ -1,8 +1,5 @@
 #include "findHull.h"
 
-using namespace cv;
-using namespace std;
-
 // defualt constructor
 findHull::findHull()
 {
@@ -11,10 +8,6 @@ findHull::findHull()
 
 void findHull::thresh_callback(cv::Mat background_removed)
 {
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	int largest_C_area = 0;
-	int largest_C_index = 0;
 
 	/// Detect edges using Threshold
 	threshold(background_removed, threshold_output, 0, 255, THRESH_OTSU);
@@ -26,10 +19,25 @@ void findHull::thresh_callback(cv::Mat background_removed)
 
 	/// Find the convex hull object for each contour
 	vector<vector<Point> >hull(contours.size());
+	vector<vector<Vec4i> >defects(contours.size());
+	vector<vector<int> >inthull(contours.size());
+
 	for (int i = 0; i < contours.size(); i++)
 	{
 		convexHull(Mat(contours[i]), hull[i], false);
+
+		// This is for finding defects - something is wrong though.
+		convexHull(Mat(contours[i]), inthull[i], false);
+		if (inthull[i].size() > 3) {
+			convexityDefects(contours[i], inthull[i], defects[i]);
+		}
 	}
+
+	// Save contours, hull and defects in public variables.
+	// Can now access from objects --> drawing class?
+	conv_hull = hull;
+	conv_defects = defects;
+	imgcontours = contours;
 
 	// Iterate through contours and find largest one
 	for (int i = 0; i < contours.size(); i++)
@@ -51,10 +59,14 @@ void findHull::thresh_callback(cv::Mat background_removed)
 
 	// Draw contours + hull results in this mat.
 	drawing = Mat::zeros(threshold_output.size(), CV_8UC3);
-	
 
-	drawContours(drawing, contours, largest_C_index, colorBlue, 2, 8, vector<Vec4i>(), 0, Point());
-	drawContours(drawing, hull, largest_C_index, colorRed, 2, 8, vector<Vec4i>(), 0, Point());
+	// Added if check to ensure that index of largest contour always is less than contour.size().
+	// I would guess it have something to do with continous updates. 
+	// Any ideas? Not strictly necessary at the moment.
+	if (contours.size() > 0 && largest_C_index < contours.size()) {
+		drawContours(drawing, contours, largest_C_index, colorBlue, 2, 8, vector<Vec4i>(), 0, Point());
+		drawContours(drawing, hull, largest_C_index, colorRed, 2, 8, vector<Vec4i>(), 0, Point());
+	}
 
 	/* This loop draws every contour.
 	for (int i = 0; i < contours.size(); i++)
--- source/FindHull.h
+++ source/FindHull.h
@@ -1,12 +1,24 @@
 #pragma once
 #include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
 class findHull
 {
 private:
 	cv::Mat threshold_output;
-
+	vector<vector<Point> > contours;
+	vector<Vec4i> hierarchy;
+	int largest_C_area = 0;
+	int largest_C_index = 0;
 
 public:
+	vector<vector<Point> > imgcontours; // storing contours
+	vector<vector<Point> > conv_hull; // storing convex hull
+	vector<vector<Vec4i> > conv_defects; // storign convex defects
+
+
 	cv::Mat drawing;
 	void thresh_callback(cv::Mat background_removed);
 
