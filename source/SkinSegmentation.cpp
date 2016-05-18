183a323b88d5d9caf6bd429de148b466f175ad74
MarieRoald <roald.marie@gmail.com>
2016-05-18 17:32:12 +0200

    Added new class

---
+++ source/SkinSegmentation.cpp
@@ -0,0 +1,11 @@
+#include "SkinSegmentation.h"
+
+
+SkinSegmentation::SkinSegmentation()
+{
+}
+
+
+SkinSegmentation::~SkinSegmentation()
+{
+}
---
+++ source/SkinSegmentation.h
@@ -0,0 +1,28 @@
+#pragma once
+#include "opencv2/opencv.hpp"
+
+using namespace cv;
+using namespace std;
+
+class SkinSegmentation
+{
+	std::vector< cv::Rect > faces;
+
+	int Y_min = 54; int Y_max = 200;
+	int Cr_min = 140; int Cr_max = 180;
+	int Cb_min = 68; int Cb_max = 136;
+	float Y_range = 150;
+	float Cr_range = 50;
+	float Cb_range = 10;
+
+public:
+	SkinSegmentation();
+	~SkinSegmentation();
+
+
+	void find_faces(cv::Mat image, std::vector< cv::Rect >& faces, cv::CascadeClassifier face_cascade);
+	void draw_faces(cv::Mat& image, std::vector< cv::Rect >faces);
+	void delete_faces(Mat thresholded_image, Mat& output_image, std::vector< cv::Rect > faces, bool debug);
+	void skin_segmentation(Mat frame, Mat& output_image);
+};
+
